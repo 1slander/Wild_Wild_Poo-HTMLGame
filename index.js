43915a46f6e06006348be00f3ab4140d38f1ba4a
1slander <hectorgomendez@gmail.com>
2023-03-09 18:56:29 +0000

    add collision check

--- index.js
+++ index.js
@@ -40,16 +40,12 @@ window.addEventListener("load", function () {
       this.x = this.game.width;
       this.speedX = 1;
       this.scale = 1;
-      this.cross = false;
-      // this.image=new Image();
-      // this.img.src=imgSrc;
-      // this.width=this.image.width/scale;
-      // this.height=this.image.height/scale;
+      this.dead = false;
     }
     update() {
       this.x -= this.speedX;
       if (this.x + this.width < 0) {
-        this.cross = true;
+        this.dead = true;
       }
     }
 
@@ -64,7 +60,13 @@ window.addEventListener("load", function () {
       super(game);
       this.width = 30;
       this.height = 60;
+      this.enemyHp = 1;
+      this.score = this.enemyHp;
       this.y = Math.floor(Math.random() * this.game.height * 0.9);
+      // this.image=new Image();
+      // this.img.src=imgSrc;
+      // this.width=this.image.width/scale;
+      // this.height=this.image.height/scale;
     }
   }
 
@@ -73,9 +75,15 @@ window.addEventListener("load", function () {
       super(game);
       this.width = 60;
       this.height = 30;
+      this.enemyHp = 3;
+      this.score = this.enemyHp;
       this.y = Math.floor(
         Math.random() * (this.game.height * 0.9 - this.height)
       );
+      // this.image=new Image();
+      // this.img.src=imgSrc;
+      // this.width=this.image.width/scale;
+      // this.height=this.image.height/scale;
     }
   }
   class Bullets {
@@ -87,6 +95,7 @@ window.addEventListener("load", function () {
       this.height = 20;
       this.speedX = 5;
       this.scale = 1;
+      this.delete = false;
       // this.image=new Image();
       // this.img.src=imgSrc;
       // this.width=this.image.width/scale;
@@ -94,6 +103,7 @@ window.addEventListener("load", function () {
     }
     update() {
       this.x += this.speedX;
+      if (this.x > this.game.width * 0.9) this.delete = true;
     }
 
     draw(context) {
@@ -113,6 +123,7 @@ window.addEventListener("load", function () {
       this.maxSpeed = 5;
       this.scale = 1;
       this.pooBullets = [];
+
       // this.image=new Image();
       // this.img.src=imgSrc;
       // this.width=this.image.width/scale;
@@ -138,6 +149,9 @@ window.addEventListener("load", function () {
       this.pooBullets.forEach((poobullet) => {
         poobullet.update();
       });
+      this.pooBullets = this.pooBullets.filter(
+        (pooBullet) => !pooBullet.delete
+      );
     }
 
     draw(context) {
@@ -164,6 +178,7 @@ window.addEventListener("load", function () {
       this.width = width;
       this.height = height;
       this.player = new Player(this);
+      this.playerHp = 3;
       this.inputs = new Inputs(this);
       this.bullets = new Bullets(this);
       this.pooBullets = 20;
@@ -173,23 +188,47 @@ window.addEventListener("load", function () {
       this.enemyTimer = 0;
       this.enemyInterval = 1000;
       this.keys = [];
+      this.gameOver == false;
+      this.score = 0;
+      this.maxScore = 50;
     }
     update(delta) {
       this.player.update();
+
       //Enemies
       this.enemies.forEach((enemy) => {
         enemy.update();
+
+        if (this.checkCollision(this.player, enemy)) {
+          enemy.dead = true;
+          this.playerHp--;
+          if (this.playerHp === 0) {
+            this.gameOver = true;
+            console.log("YOU LOST");
+          }
+        }
+        this.player.pooBullets.forEach((bullet) => {
+          if (this.checkCollision(bullet, enemy)) {
+            enemy.enemyHp--;
+            bullet.delete = true;
+            if (enemy.enemyHp === 0) {
+              enemy.dead = true;
+              this.score += enemy.score;
+              console.log(this.score);
+            }
+          }
+        });
       });
-      this.enemies.filter((enemy) => {
-        !enemy.cross;
-      });
-      console.log(this.enemies);
+
+      this.enemies = this.enemies.filter((enemy) => !enemy.dead);
+
       if (this.enemyTimer > this.enemyInterval) {
         this.addEnemies();
         this.enemyTimer = 0;
       } else {
         this.enemyTimer += delta;
       }
+
       //Bullets
       if (this.pooBullets === 0) {
         //this.incrementBullets();
@@ -212,11 +251,20 @@ window.addEventListener("load", function () {
 
     addEnemies() {
       const chooseEnemy = Math.random() * 5;
-      if (chooseEnemy < 3) {
+      if (chooseEnemy < 4) {
         this.enemies.push(new Paper(this));
       } else if (chooseEnemy < 5) {
         this.enemies.push(new Soap(this));
       }
+    }
+
+    checkCollision(obj1, obj2) {
+      return (
+        obj1.x < obj2.x + obj2.width &&
+        obj1.x + obj1.width > obj2.x &&
+        obj1.y < obj2.y + obj2.height &&
+        obj1.y + obj1.height > obj2.y
+      );
     }
   }
 
@@ -225,7 +273,7 @@ window.addEventListener("load", function () {
   let lastTime = 0;
   function startGame(time) {
     const delta = time - lastTime;
-    console.log(delta);
+    //console.log(delta);
     lastTime = time;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
